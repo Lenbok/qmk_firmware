9c5eb1813fa58050f78351bbd6bae12f6c30f943
Lenbok <lenbok@gmail.com>
2018-05-12 00:40:49 +1200

    Make this like the others

--- keyboards/redox/redox.h
+++ keyboards/redox/redox.h
@@ -28,7 +28,7 @@
     L30, L31, L32, L33, L34, L35, L36, R30, R31, R32, R33, R34, R35, R36, \
     L40, L41, L42, L43, L44, L45, L46, R40, R41, R42, R43, R44, R45, R46 \
     ) \
-    KEYMAP( \
+    LAYOUT( \
         KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, KC_##L05, KC_##L06, KC_##R00, KC_##R01, KC_##R02, KC_##R03, KC_##R04, KC_##R05, KC_##R06, \
         KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15, KC_##L16, KC_##R10, KC_##R11, KC_##R12, KC_##R13, KC_##R14, KC_##R15, KC_##R16, \
         KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25, KC_##L26, KC_##R20, KC_##R21, KC_##R22, KC_##R23, KC_##R24, KC_##R25, KC_##R26, \
