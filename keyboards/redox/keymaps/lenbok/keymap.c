c9930e64622a44427b1a7f93d288dbbfbadb48bd
Len Trigg <len@realtimegenomics.com>
2019-02-05 17:20:11 +1300

    LGUI is now available on the same key as GRV.

--- keyboards/redox/keymaps/lenbok/keymap.c
+++ keyboards/redox/keymaps/lenbok/keymap.c
@@ -27,6 +27,7 @@ enum custom_keycodes {
 #define LOWR_EN LT(_LOWER,KC_ENT)
 #define RASE_SP LT(_RAISE,KC_SPC)
 #define KC_CTTB CTL_T(KC_TAB)
+#define KC_GUGR LGUI_T(KC_GRV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -34,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------.                                       ,--------+--------+--------+--------+--------+--------.
       KC_GRV ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                          KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------+--------|
-      KC_GRV ,  KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  ,  KC_T  , KC_MINS,                       KC_EQL ,  KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  ,  KC_P  , KC_BSPC,
+      KC_GUGR,  KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  ,  KC_T  , KC_MINS,                       KC_EQL ,  KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  ,  KC_P  , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------+--------|
       KC_CTTB,  KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  , KC_LBRC,                       KC_RBRC,  KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------+--------+--------|
