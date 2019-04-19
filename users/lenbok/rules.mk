3ddbe286cbc6810734a27aa96c5579ea05b6bd0c
Lenbok <lenbok@gmail.com>
2019-04-19 19:43:12 +1200

    My rgb idle timeout supports rgblight (blank) and rgb_matrix (mode change).

--- users/lenbok/rules.mk
+++ users/lenbok/rules.mk
@@ -1,3 +1,9 @@
+RGB_IDLE = yes   # If yes do something with the rgblight/rgb_matrix after inactivity timeout
+
 SRC += lenbok.c
 
 EXTRAFLAGS += -flto
+
+ifeq ($(strip $(RGB_IDLE)), yes)
+    OPT_DEFS += -DRGB_IDLE
+endif
