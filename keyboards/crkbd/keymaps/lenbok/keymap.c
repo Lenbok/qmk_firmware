cc86a784dce07981421cdd585b66fe1479a0206f
Lenbok <lenbok@gmail.com>
2019-04-20 13:26:54 +1200

    Indentation for coding conventions.

--- keyboards/crkbd/keymaps/lenbok/keymap.c
+++ keyboards/crkbd/keymaps/lenbok/keymap.c
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void matrix_init_user(void) {
     //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-    #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
-    #endif
+#ifdef SSD1306OLED
+    iota_gfx_init(!has_usb());   // turns on the display
+#endif
 }
 
 //SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
@@ -115,18 +115,18 @@ const char* read_layer_state(void) {
 #ifdef RGBLIGHT_ENABLE
 char rgbl_info_str[24];
 const char *read_rgblight_info(void) {
-  snprintf(rgbl_info_str, sizeof(rgbl_info_str), "RGBl: %s Mode: %2d ",
-           rgblight_config.enable ? "On  " : "Off ", rgblight_config.mode);
-  return rgbl_info_str;
+    snprintf(rgbl_info_str, sizeof(rgbl_info_str), "RGBl: %s Mode: %2d ",
+             rgblight_config.enable ? "On  " : "Off ", rgblight_config.mode);
+    return rgbl_info_str;
 }
 #endif
 #ifdef RGB_MATRIX_ENABLE
 char rgbm_info_str[24];
 const char *read_rgb_matrix_info(void) {
     uint8_t mode = rgb_matrix_get_mode();
-  snprintf(rgbm_info_str, sizeof(rgbm_info_str), "RGBm: %s Mode: %2d ",
-           rgb_matrix_config.enable ? "On  " : "Off ", mode);
-  return rgbm_info_str;
+    snprintf(rgbm_info_str, sizeof(rgbm_info_str), "RGBm: %s Mode: %2d ",
+             rgb_matrix_config.enable ? "On  " : "Off ", mode);
+    return rgbm_info_str;
 }
 #endif
 
@@ -135,47 +135,47 @@ void matrix_scan_keymap(void) {
 }
 
 void matrix_render_user(struct CharacterMatrix *matrix) {
-  if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    // Note: the fourth row should use matrix_write, not matrix_write_ln, to prevent wrap
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    matrix_write_ln(matrix, read_keylogs());
+    if (is_master) {
+        // If you want to change the display of OLED, you need to change here
+        // Note: the fourth row should use matrix_write, not matrix_write_ln, to prevent wrap
+        matrix_write_ln(matrix, read_layer_state());
+        matrix_write_ln(matrix, read_keylog());
+        matrix_write_ln(matrix, read_keylogs());
 #ifdef RGBLIGHT_ENABLE
-    matrix_write(matrix, read_rgblight_info());
+        matrix_write(matrix, read_rgblight_info());
 #endif
 #ifdef RGB_MATRIX_ENABLE
-    matrix_write(matrix, read_rgb_matrix_info());
+        matrix_write(matrix, read_rgb_matrix_info());
 #endif
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
-  } else {
-    matrix_write(matrix, read_logo());
-  }
+        //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+        //matrix_write_ln(matrix, read_host_led_state());
+        //matrix_write_ln(matrix, read_timelog());
+    } else {
+        matrix_write(matrix, read_logo());
+    }
 }
 
 void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
+    if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+        memcpy(dest->display, source->display, sizeof(dest->display));
+        dest->dirty = true;
+    }
 }
 
 void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
+    struct CharacterMatrix matrix;
+    matrix_clear(&matrix);
+    matrix_render_user(&matrix);
+    matrix_update(&display, &matrix);
 }
 #endif//SSD1306OLED
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
+    if (record->event.pressed) {
 #ifdef SSD1306OLED
-    set_keylog(keycode, record);
+        set_keylog(keycode, record);
 #endif
-    // set_timelog();
-  }
-  return true;
+        // set_timelog();
+    }
+    return true;
 }
--- users/lenbok/lenbok.c
+++ users/lenbok/lenbok.c
@@ -90,12 +90,12 @@ void matrix_scan_user(void) {
 
 __attribute__ ((weak))
 uint32_t layer_state_set_keymap(uint32_t state) {
-  return state;
+    return state;
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
-  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  return layer_state_set_keymap(state);
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+    return layer_state_set_keymap(state);
 }
 
 
