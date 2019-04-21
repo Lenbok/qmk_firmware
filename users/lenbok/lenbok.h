3932fabd5a3eec5d650ab9ab7220b61002d5538d
Lenbok <lenbok@gmail.com>
2019-04-22 10:03:16 +1200

    Ugnh, forgot I need shift on adjust layer for reverse stepping rgb modes.

--- users/lenbok/lenbok.h
+++ users/lenbok/lenbok.h
@@ -74,8 +74,8 @@ enum custom_keycodes {
 #define ___ADJUST_L2___  KC_CAPS, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD
 #define ___ADJUST_L3___  KC_LSFT, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_TOG
 #define ___ADJUST_R1___                                                        KC_MUTE, KC_HOME, KC_UP  , KC_END , _______,  KC_INS
-#define ___ADJUST_R2___                                                        KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______
-#define ___ADJUST_R3___                                                        KC_VOLD, _______, _______, _______, _______, VLK_TOG
+#define ___ADJUST_R2___                                                        KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, _______, VLK_TOG
+#define ___ADJUST_R3___                                                        KC_VOLD, _______, _______, _______, _______, _______
 
 
 #define ___BASE_6_MOD___                            KC_LALT, KC_ENT,  KC_LOWR, KC_RASE,  KC_SPC, KC_RCTL
